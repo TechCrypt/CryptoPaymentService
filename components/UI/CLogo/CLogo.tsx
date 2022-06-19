c9adcbc2c0779016d0d07f184bb895d34d638082
test <zhasik.asik01@gmail.com>
2022-06-19 15:32:06 +0600

    perf

--- components/UI/CLogo/CLogo.tsx
+++ components/UI/CLogo/CLogo.tsx
@@ -1,12 +1,29 @@
+/** @jsxRuntime classic /
+ /* @jsx jsx */
+import {css, jsx} from '@emotion/react'
 import styled from '@emotion/styled'
 
 
-export const CLogo = styled('span')`
+const CLogoStyled = styled('span')`
   display: inline-block;
-  background: url("/assets/img/logo.svg");
   width: 133.4px;
   height: 64px;
+
   &:hover {
     cursor: pointer;
   }
 `
+
+export const CLogo = () => {
+    return <h2 css={css`
+      display: inline-block;
+      width: 133.4px;
+      height: 64px;
+
+      &:hover {
+        cursor: pointer;
+      }
+    `}>
+        COMM
+    </h2>
+}
