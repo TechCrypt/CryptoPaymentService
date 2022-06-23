b1ffe80eef6eacd64d712bcfc18913bb4fb8a5e6
test <zhasik.asik01@gmail.com>
2022-06-23 15:20:37 +0600

    Revert "perf"
    This reverts commit c9adcbc2c0779016d0d07f184bb895d34d638082.

--- components/UI/CLogo/CLogo.tsx
+++ components/UI/CLogo/CLogo.tsx
@@ -1,29 +1,12 @@
-/** @jsxRuntime classic /
- /* @jsx jsx */
-import {css, jsx} from '@emotion/react'
 import styled from '@emotion/styled'
 
 
-const CLogoStyled = styled('span')`
+export const CLogo = styled('span')`
   display: inline-block;
+  background: url("/assets/img/logo.svg");
   width: 133.4px;
   height: 64px;
-
   &:hover {
     cursor: pointer;
   }
 `
-
-export const CLogo = () => {
-    return <h2 css={css`
-      display: inline-block;
-      width: 133.4px;
-      height: 64px;
-
-      &:hover {
-        cursor: pointer;
-      }
-    `}>
-        COMM
-    </h2>
-}
